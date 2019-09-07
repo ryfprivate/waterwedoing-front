7fc35d24382a1b8b52c2f3df21c75d760b23fe0c
rayfeng <ryfprivate@outlook.com>
2019-09-07 18:18:21 +1000

    Added static styling for Button

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import styled from 'styled-components'
+
+const StyledButton = styled(Button)`
+    && {
+        margin: 13px;
+    }
+`
 
 const BasicButton = ({ label, color }) => {
     return (
-        <Button variant="outlined" color={color}>
+        <StyledButton variant="outlined" color={color}>
             {label}
-        </Button>
+        </StyledButton>
     )
 }
 
