eb3504c6cd05f1d8e1f4da24975ed4c0916aeeb6
juntao-dev <juntao.dev@gmail.com>
2019-09-07 17:14:15 +1000

    chore/update page layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from "react";
 import Header from "./Components/Header";
+import Home from "./Components/Home";
 import logo from "./logo.svg";
 import "./App.css";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Page = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+`;
+
+const PageBody = styled.div`
+  /* margin-top: 10vh; */
+  /* border: 1px solid blue; */
+  /* height: 80vh; */
+`;
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      {/* <Page className="App"> */}
+      <Page>
         <Header />
-      </div>
+        <PageBody>
+          <Route exact path="/" component={Home} />
+        </PageBody>
+      </Page>
     </Router>
   );
 }
