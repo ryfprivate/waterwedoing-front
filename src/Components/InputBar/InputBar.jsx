0f99f655e99f46c52c33b96cab8fe158142d595b
rayfeng <ryfprivate@outlook.com>
2019-09-07 18:18:39 +1000

    Added styling for InputBar

--- src/Components/InputBar/InputBar.jsx
+++ src/Components/InputBar/InputBar.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import Button from '../Button'
 import TextInput from '../TextInput'
+import styled from 'styled-components'
+
+const StyledButton = styled(Button)`
+    && {
+        margin: auto;
+        color: green;
+        background-color: black;
+    }
+`
+
+const StyledTextInput = styled(TextInput)`
+    && {
+        color: green;
+    }
+`
+
+const Wrapper = styled.div`
+    margin-top: 15%;
+`
 
 const InputBar = () => {
     return (
-        <div>
-            <TextInput label="Input asdfa" />
-            <Button label="Go" color="default"></Button>
-        </div>
+        <Wrapper>
+            <StyledTextInput label="Input asdfa" />
+            <StyledButton label="Go" color="default" />
+        </Wrapper>
     )
 }
 
