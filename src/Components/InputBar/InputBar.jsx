6960b153b9486701f3a1362e5d3b9c00fee5d1f1
rayfeng <ryfprivate@outlook.com>
2019-09-07 15:07:04 +1000

    Added basic InputBar

---
+++ src/Components/InputBar/InputBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '../Button'
+import TextInput from '../TextInput'
+
+const InputBar = () => {
+    return (
+        <div>
+            <TextInput label="Input asdfa" />
+            <Button label="Go" color="default"></Button>
+        </div>
+    )
+}
+
+export default InputBar
