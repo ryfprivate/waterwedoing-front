48af9be55f82644e447fd05203b7c3bb88245c54
rayfeng <ryfprivate@outlook.com>
2019-09-08 11:54:25 +1000

    Added Submit Button to FormInput

--- src/Components/FormInput/FormInput.jsx
+++ src/Components/FormInput/FormInput.jsx
@@ -5,7 +5,7 @@ import InputLabel from '@material-ui/core/InputLabel'
 import MenuItem from '@material-ui/core/MenuItem'
 import FormControl from '@material-ui/core/FormControl'
 import Select from '@material-ui/core/Select'
-import Button from '@material-ui/core/Button'
+import Button from '../Button'
 
 const suburbArray = []
 
@@ -72,6 +72,7 @@ const FormInput = () => {
                     })}
                 </Select>
             </FormControl>
+            <Button className={classes.button}>Submit</Button>
         </form>
     )
 }
